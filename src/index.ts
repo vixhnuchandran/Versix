03c3bd8ba85276b0155bfd4286d52016c2216773
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-15 05:22:47 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import app from "./app"
+import dotenv from "dotenv"
+import sequelize, { checkConnection } from "./configs/db.config"
+import { Store } from "./models"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8484
+
+const startServer = async () => {
+  try {
+    await checkConnection()
+
+    sequelize
+      .sync({ alter: true })
+      .then(() => {
+        console.log("Pipeline Database synchronized")
+        Store.sync({ alter: true })
+      })
+      .then(() => {
+        console.log("Store Table synchronized")
+        app.listen(PORT, () => {
+          console.log(`Server listening on port ${PORT}`)
+        })
+      })
+      .catch(error => {
+        console.error("Database synchronization failed:", error)
+      })
+  } catch (error) {
+    console.error("Failed to start server:", error)
+    process.exit(1)
+  }
+}
+
+startServer()
