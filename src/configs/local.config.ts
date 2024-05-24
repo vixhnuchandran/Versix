2ce6678c957429a998cab65699f84f7a1baf7a2c
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-24 21:15:05 +0530

    Add script to ensure local_store directory exists

---
+++ src/configs/local.config.ts
@@ -0,0 +1,14 @@
+import { existsSync, mkdirSync } from "fs"
+import { join } from "path"
+
+const mainFolderName = "local_store"
+const mainFolderPath = join(process.cwd(), mainFolderName)
+
+export const isLocalFolderExist = async () => {
+  if (!existsSync(mainFolderPath)) {
+    mkdirSync(mainFolderPath)
+    console.log(`Folder '${mainFolderName}' created successfully.`)
+  } else {
+    console.log(`Folder '${mainFolderName}' already exists.`)
+  }
+}
