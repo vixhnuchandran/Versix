e990fe6f91e66291914a9ae62c67f6cec7e1fb7f
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-24 22:47:08 +0530

    Use filestore class

--- src/routes/handler.ts
+++ src/routes/handler.ts
@@ -1,13 +1,6 @@
 import { Request, Response } from "express"
 import { StatusCodes as HTTP_CODE } from "http-status-codes"
-import {
-  existsSync,
-  mkdirSync,
-  readdirSync,
-  readFileSync,
-  writeFileSync,
-} from "fs"
-import { join } from "path"
+import { fileStore } from "../utils/fileStore"
 import {
   validateGetDataReq,
   validateSetDataReq,
@@ -16,6 +9,8 @@ import {
   ValidationResult,
 } from "../validations"
 import * as crypto from "crypto"
+
+const fileStorage = new fileStore()
 
 // Set Data
 export const setData = async (req: Request, res: Response) => {
@@ -32,50 +27,16 @@ export const setData = async (req: Request, res: Response) => {
 
   const hashedId = crypto.createHash("md5").update(id).digest("hex")
 
-  const folderPath = join(process.cwd(), "local_store", hashedId, name)
   try {
-    mkdirSync(folderPath, { recursive: true })
-    console.log(`Folder '${name}' created successfully.`)
+    const version = await fileStorage.saveData(hashedId, name, data)
+    console.log(`Data saved successfully with version ${version}.`)
+    return res.status(HTTP_CODE.OK).json({ version })
   } catch (error) {
-    console.error(`Error creating folder '${name}':`, error)
+    console.error("Error saving data:", error)
     return res
       .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
       .json({ error: "INTERNAL_SERVER_ERROR" })
   }
-
-  let fileName: string
-  let version: number
-  try {
-    const files = readdirSync(folderPath)
-    if (files.length === 0) {
-      fileName = "1.json"
-      version = 1
-    } else {
-      const fileNumbers = files
-        .map(file => parseInt(file.split(".json")[0], 10))
-        .filter(Number.isInteger)
-      version = Math.max(...fileNumbers) + 1
-      fileName = `${version}.json`
-    }
-  } catch (error) {
-    console.error(`Error reading directory '${folderPath}':`, error)
-    return res
-      .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
-      .json({ error: "INTERNAL_SERVER_ERROR" })
-  }
-
-  const filePath = join(folderPath, fileName)
-  try {
-    writeFileSync(filePath, JSON.stringify(data, null, 2))
-    console.log(`Data saved to '${fileName}' successfully.`)
-  } catch (error) {
-    console.error(`Error writing file '${fileName}':`, error)
-    return res
-      .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
-      .json({ error: "INTERNAL_SERVER_ERROR" })
-  }
-
-  return res.status(HTTP_CODE.OK).json({ version })
 }
 
 // Get Data
@@ -97,54 +58,13 @@ export const getData = async (req: Request, res: Response) => {
 
   const hashedId = crypto.createHash("md5").update(id).digest("hex")
 
-  const folderPath = join(process.cwd(), "local_store", hashedId, name)
-
-  if (!existsSync(folderPath)) {
-    return res.status(HTTP_CODE.NOT_FOUND).json({ error: "Folder not found." })
-  }
-
-  let fileName: string
-
   try {
-    const files = readdirSync(folderPath).filter(file => file.endsWith(".json"))
-    if (files.length === 0) {
-      return res
-        .status(HTTP_CODE.NOT_FOUND)
-        .json({ error: "No data files found." })
-    }
-
-    if (version) {
-      fileName = `${version}.json`
-      if (!files.includes(fileName)) {
-        return res
-          .status(HTTP_CODE.NOT_FOUND)
-          .json({ error: `Version ${version} not found.` })
-      }
-    } else {
-      const fileNumbers = files
-        .map(file => parseInt(file.split(".json")[0], 10))
-        .filter(Number.isInteger)
-      const latestVersion = Math.max(...fileNumbers)
-      fileName = `${latestVersion}.json`
-    }
+    const data = await fileStorage.getData(hashedId, name, version)
+    console.log("Data retrieved successfully.")
+    return res.status(HTTP_CODE.OK).json(data)
   } catch (error) {
-    console.error(`Error reading directory '${folderPath}':`, error)
-    return res
-      .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
-      .json({ error: "INTERNAL_SERVER_ERROR" })
-  }
-
-  const filePath = join(folderPath, fileName)
-
-  try {
-    const fileData = readFileSync(filePath, "utf8")
-    const parsedData = JSON.parse(fileData)
-    return res.status(HTTP_CODE.OK).json(parsedData)
-  } catch (error) {
-    console.error(`Error reading file '${fileName}':`, error)
-    return res
-      .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
-      .json({ error: "INTERNAL_SERVER_ERROR" })
+    console.error("Error retrieving data:", error.message)
+    return res.status(HTTP_CODE.NOT_FOUND).json({ error: "Not Found" })
   }
 }
 
@@ -163,23 +83,14 @@ export const hasData = async (req: Request, res: Response) => {
 
   const hashedId = crypto.createHash("md5").update(id).digest("hex")
 
-  const folderPath = join(process.cwd(), "local_store", hashedId, name)
-
-  if (existsSync(folderPath)) {
-    try {
-      const files = readdirSync(folderPath)
-      if (files.length > 0) {
-        return res.status(HTTP_CODE.OK).json({ data: true })
-      } else {
-        return res.status(HTTP_CODE.OK).json({ data: false })
-      }
-    } catch (error) {
-      console.error(`Error reading directory '${folderPath}':`, error)
-      return res
-        .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
-        .json({ error: "INTERNAL_SERVER_ERROR" })
-    }
-  } else {
-    return res.status(HTTP_CODE.OK).json({ data: false })
+  try {
+    const hasData = await fileStorage.hasData(hashedId, name)
+    console.log("Check for data completed.")
+    return res.status(HTTP_CODE.OK).json({ data: hasData })
+  } catch (error) {
+    console.error("Error checking for data:", error)
+    return res
+      .status(HTTP_CODE.INTERNAL_SERVER_ERROR)
+      .json({ error: "INTERNAL_SERVER_ERROR" })
   }
 }
