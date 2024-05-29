4a14930a1934aa6cc0923582119068eb7f145e58
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-30 03:49:15 +0530

    Fix auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,24 @@
 import { Request, Response, NextFunction } from "express"
 import { StatusCodes as HTTP_CODE } from "http-status-codes"
+import dotenv from "dotenv"
+import fs from "fs"
+import path from "path"
 
-const validApiKeys = ["apikey1", "apikey2", "apikey3"]
+dotenv.config()
+
+const keysFilePath = path.resolve(__dirname, "../", process.env.API_KEYS_FILE!)
+let validApiKeys: string[] = []
+
+try {
+  const keysFileContent = fs.readFileSync(keysFilePath, "utf-8")
+
+  validApiKeys = keysFileContent
+    .split("\n")
+    .map(key => key.trim())
+    .filter(key => key.length > 0)
+} catch (error) {
+  console.error("Error reading API keys file:", error)
+}
 
 export const authMiddleware = (
   req: Request,
@@ -10,7 +27,7 @@ export const authMiddleware = (
 ) => {
   const apiKey = req.headers["x-api-key"]
 
-  if (!apiKey || typeof apiKey !== "string" || !validApiKeys.includes(apiKey)) {
+  if (typeof apiKey !== "string" || !validApiKeys.includes(apiKey)) {
     return res.status(HTTP_CODE.FORBIDDEN).json({ error: "Not authorized" })
   }
 
