3e092f6f8cf8458a44f87387081099ed02b13fdf
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-29 05:43:32 +0530

    Refactor 'isLocalFolderExist' function to return a boolean - Modify 'isLocalFolderExist' function signature to explicitly return a Promise<boolean>. - Update function implementation to return 'true' after folder creation or if folder already exists.

--- src/configs/local.config.ts
+++ src/configs/local.config.ts
@@ -4,11 +4,12 @@ import { join } from "path"
 const mainFolderName = "local_store"
 const mainFolderPath = join(process.cwd(), mainFolderName)
 
-export const isLocalFolderExist = async () => {
+export const isLocalFolderExist = async (): Promise<boolean> => {
   if (!existsSync(mainFolderPath)) {
     mkdirSync(mainFolderPath)
     console.log(`Folder '${mainFolderName}' created successfully.`)
   } else {
     console.log(`Folder '${mainFolderName}' already exists.`)
   }
+  return true
 }
